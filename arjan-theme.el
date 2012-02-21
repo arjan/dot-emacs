63b33ab505330f54a8e01f2c5174d9670b177e11
Arjan Scherpenisse <arjan@scherpenisse.net>
2012-02-21 10:22:54 +0100

    theme

---
+++ arjan-theme.el
@@ -0,0 +1,12 @@
+(deftheme arjan
+  "Created 2011-12-05.")
+
+(custom-theme-set-variables
+ 'arjan
+ '(custom-safe-themes (quote ("1d2126921f639e9e3b9275617555729583578ded" default))))
+
+(custom-theme-set-faces
+ 'arjan
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "unknown" :family "Ubuntu Mono")))))
+
+(provide-theme 'arjan)
